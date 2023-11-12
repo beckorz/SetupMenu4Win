5bd094e1a84dd7ba10028e36eda4ead0c1945584
beckorz <beckorz+github@gmail.com>
2023-11-12 15:20:03 +0900

    👍[Add] ECMAScript262_5th.js 追加

---
+++ _lib/ECMAScript262_5th.js
@@ -0,0 +1,203 @@
+﻿// array.prototype.filter
+if (!Array.prototype.filter) {
+  Array.prototype.filter = function(fun/*, thisArg*/) {
+    'use strict';
+
+    if (this === void 0 || this === null) {
+      throw new TypeError();
+    }
+
+    var t = Object(this);
+    var len = t.length >>> 0;
+    if (typeof fun !== 'function') {
+      throw new TypeError();
+    }
+
+    var res = [];
+    var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+    for (var i = 0; i < len; i++) {
+      if (i in t) {
+        var val = t[i];
+
+        // NOTE: Technically this should Object.defineProperty at
+        //       the next index, as push can be affected by
+        //       properties on Object.prototype and Array.prototype.
+        //       But that method's new, and collisions should be
+        //       rare, so use the more-compatible alternative.
+        if (fun.call(thisArg, val, i, t)) {
+          res.push(val);
+        }
+      }
+    }
+
+    return res;
+  };
+}
+
+// array.prototype.indexOf
+
+// Production steps of ECMA-262, Edition 5, 15.4.4.14
+// Reference: http://es5.github.io/#x15.4.4.14
+if (!Array.prototype.indexOf) {
+  Array.prototype.indexOf = function(searchElement, fromIndex) {
+
+    var k;
+
+    // 1. Let o be the result of calling ToObject passing
+    //    the this value as the argument.
+    if (this === null) {
+      throw new TypeError('"this" is null or not defined');
+    }
+
+    var o = Object(this);
+
+    // 2. Let lenValue be the result of calling the Get
+    //    internal method of o with the argument "length".
+    // 3. Let len be ToUint32(lenValue).
+    var len = o.length >>> 0;
+
+    // 4. If len is 0, return -1.
+    if (len === 0) {
+      return -1;
+    }
+
+    // 5. If argument fromIndex was passed let n be
+    //    ToInteger(fromIndex); else let n be 0.
+    var n = +fromIndex || 0;
+
+    if (Math.abs(n) === Infinity) {
+      n = 0;
+    }
+
+    // 6. If n >= len, return -1.
+    if (n >= len) {
+      return -1;
+    }
+
+    // 7. If n >= 0, then Let k be n.
+    // 8. Else, n<0, Let k be len - abs(n).
+    //    If k is less than 0, then let k be 0.
+    k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+
+    // 9. Repeat, while k < len
+    while (k < len) {
+      // a. Let Pk be ToString(k).
+      //   This is implicit for LHS operands of the in operator
+      // b. Let kPresent be the result of calling the
+      //    HasProperty internal method of o with argument Pk.
+      //   This step can be combined with c
+      // c. If kPresent is true, then
+      //    i.  Let elementK be the result of calling the Get
+      //        internal method of o with the argument ToString(k).
+      //   ii.  Let same be the result of applying the
+      //        Strict Equality Comparison Algorithm to
+      //        searchElement and elementK.
+      //  iii.  If same is true, return k.
+      if (k in o && o[k] === searchElement) {
+        return k;
+      }
+      k++;
+    }
+    return -1;
+  };
+}
+
+// array.prototype.lastIndexOf
+
+// Production steps of ECMA-262, Edition 5, 15.4.4.15
+// Reference: http://es5.github.io/#x15.4.4.15
+if (!Array.prototype.lastIndexOf) {
+  Array.prototype.lastIndexOf = function(searchElement /*, fromIndex*/) {
+    'use strict';
+
+    if (this === void 0 || this === null) {
+      throw new TypeError();
+    }
+
+    var n, k,
+      t = Object(this),
+      len = t.length >>> 0;
+    if (len === 0) {
+      return -1;
+    }
+
+    n = len - 1;
+    if (arguments.length > 1) {
+      n = Number(arguments[1]);
+      if (n !== n) {
+        n = 0;
+      }
+      else if (n !== 0 && n !== (1 / 0) && n !== -(1 / 0)) {
+        n = (n > 0 || -1) * Math.floor(Math.abs(n));
+      }
+    }
+
+    for (k = n >= 0 ? Math.min(n, len - 1) : len - Math.abs(n); k >= 0; k--) {
+      if (k in t && t[k] === searchElement) {
+        return k;
+      }
+    }
+    return -1;
+  };
+}
+
+// array.prototype.map
+if (!Array.prototype.map) {
+    Array.prototype.map = function( func ){
+        return this.reduce(function( result, item, index ){
+            result.push( func.apply( result, [ item, index ] ) );
+            return result;
+        }, []);
+    };
+}
+
+// Rubyのinjectに相当するメソッド
+// http://d.hatena.ne.jp/TipsMemo+computer-technology/20150110/p1
+if (!Array.prototype.reduce) {
+    Array.prototype.reduce = function( func, initValue ){
+        // 初期値をセット
+        var result = initValue;
+
+        // 各要素ごとに
+        this.each(function( item, index ){
+            // 結果を累積更新する
+            result = func( result, item, index );
+        });
+
+        // 累積結果を返す
+        return result;
+    };
+}
+
+// 配列のイテレータ
+if (!Array.prototype.each) {
+    Array.prototype.each = function(func) {
+        for(var i = 0; i < this.length; i++){
+            func.call(this, this[i], i);
+        }
+        return this; // チェインを継続
+    };
+}
+
+
+// オブジェクトを配列に変換
+Array.src = function( iterable, funcItem, funcLength ){
+    var arr = [];
+    
+    var length = null;
+    if( funcLength ){
+        length = funcLength(iterable);
+    }else{
+        //iterable.length;
+    }
+
+    for(var i=0; i<length; i++ ){
+        if (funcItem) {
+            arr.push(funcItem(iterable, i));
+        }else{
+            arr.push(iterable[i]);
+        }
+    }
+    return arr;
+};
+
